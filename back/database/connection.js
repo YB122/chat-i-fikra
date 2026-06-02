e379783d2fa171a752c8531e0b7f623bf3b81eaf
YB122 <youssefbenyamine2eme@gmail.com>
2026-06-02 11:47:46 +0300

    add link db

--- back/database/connection.js
+++ back/database/connection.js
@@ -6,7 +6,7 @@ dns.setServers(["8.8.8.8", "8.8.8.4"]);
 
 export const dataBaseConnection = () => {
   mongoose
-    .connect(process.env.DATA_BASE)
+    .connect("mongodb+srv://youssefbenyamineiti2025_db_user:youssef@cluster0.ax7vlz9.mongodb.net/ifikra-chat")
     .then(() => console.log("data base connected"))
     .catch((err) => console.log(err));
 };
