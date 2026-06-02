a8ea8ff1be10aea8d151fe43bac353f7e227c83e
YB122 <youssefbenyamine2eme@gmail.com>
2026-06-02 11:03:30 +0300

    put the database linl

--- back/database/connection.js
+++ back/database/connection.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.8.4"]);
 
 export const dataBaseConnection = () => {
   mongoose
-    .connect("mongodb://localhost:27017/nti-chat")
+    .connect("mongodb+srv://youssefbenyamineiti2025_db_user:youssef@cluster0.ax7vlz9.mongodb.net/?appName=Cluster0")
     .then(() => console.log("data base connected"))
     .catch((err) => console.log(err));
 };
